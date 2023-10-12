c318abce9cb384fd075b403a0f00ec810bb12445
Tanjil Ahamed <tanjil113355@gmail.com>
2023-10-12 22:22:04 +0600

    crud all method applied on this project

--- src/component/Update.jsx
+++ src/component/Update.jsx
@@ -4,12 +4,34 @@ const Update = () => {
 
     const loadUser = useLoaderData()
 
+    const handleUpdate = e => {
+        e.preventDefault()
+        const form = e.target
+        const name = form.name.value;
+        const email = form.email.value;
+
+        const user = { name, email }
+        console.log(user)
+
+        fetch(`http://localhost:5000/user/${loadUser._id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify(user)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+    }
+
     return (
         <div>
             <h2>update</h2>
             <h1>{loadUser.name}</h1>
 
-            <form>
+            <form onSubmit={handleUpdate}>
                 <h2>update your data</h2>
                 <br />
                 <br />
